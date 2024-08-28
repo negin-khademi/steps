01e9951e54d1b22f4d008ece62783bf93ef48c3d
Negin <negin.khademi@gmail.com>
2024-08-28 15:58:34 +0330

    first command

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+            <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+            <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+
+          <div className="buttons">
+            <button onClick={() => handlePrevious()} style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+              Previous
+            </button>
+            <button onClick={() => handleNext()} style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-export default App;
